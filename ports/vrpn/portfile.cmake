a8fd609c05a24fe713e81dc895bd5557b69b9422
Zachary Wartell <zwartell@uncc.edu>
2023-06-26 14:51:37 -0400

    -vrpn port - attempt - wip 2/N

--- ports/vrpn/portfile.cmake
+++ ports/vrpn/portfile.cmake
@@ -2,11 +2,11 @@
     OUT_SOURCE_PATH SOURCE_PATH
     #REPO https://github.com/vrpn/vrpn.git
     REPO vrpn/vrpn
-    REF v7.35
+    REF version_07.35
     # tar -C ./ -cf - --sort=name vrpn | sha512sum
     # ref:  https://stackoverflow.com/questions/545387/linux-compute-a-single-hash-for-a-given-folder-contents
-    SHA512 dcc9b084be6c28b3b5e89076cf4b92320151abccdb7032f15bac3c4bf60223fb2267fe6f24b2735926487d1a3c043911441e2607d95457680f5a7eaa2bb345aa
-    HEAD_REF version_07.35
+    SHA512 9a4225e01d07f1a298cd1fc5b938e4be57c03b2c82560a2a77cc327a4a83cbde6b88f796fd6071f7bd8de7ebbb6a8bac86b56fa3025697b5bb7820a7cc71862a
+    #HEAD_REF version_07.35
 )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
