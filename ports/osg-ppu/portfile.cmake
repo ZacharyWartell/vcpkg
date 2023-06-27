6e1bf4834b07153e897a2858ab6cf0858b08234b
Zachary Wartell <zwartell@uncc.edu>
2023-06-27 18:31:08 -0400

    -initial

---
+++ ports/osg-ppu/portfile.cmake
@@ -0,0 +1,20 @@
+﻿vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO cgart/osgPPU
+    # https://github.com/cgart/osgPPU.git
+    #REF version_07.35    
+    REF cb65b6b18ba23cb34fe9a939f44113c24a412422    
+    SHA512 7534120f49446b92dd9a15367b823772e5d41a1e548a2187dbdb6f3f1583341311b10d42ae41df040054c9b33d0320cc7f6fa8f67b9e8f27cd39b5dcb6659f26
+    #SHA512 9a4225e01d07f1a298cd1fc5b938e4be57c03b2c82560a2a77cc327a4a83cbde6b88f796fd6071f7bd8de7ebbb6a8bac86b56fa3025697b5bb7820a7cc71862a
+    #HEAD_REF version_07.35
+    #PATCHES
+    #    CMakeLists.txt-vrpn.patch
+    #    CMakeLists.txt-atmellib.patch
+    #   CMakeLists.txt-gpsnmealib.patch
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    )
+vcpkg_cmake_install()
+#file(INSTALL "${SOURCE_PATH}/README.Legal" DESTINATION "${CURRENT_PACKAGES_DIR}/share/vrpn" RENAME copyright)
