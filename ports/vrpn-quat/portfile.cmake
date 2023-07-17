299ba862a0cb6bdf583a8c6c180ea0b182dfc93b
Zachary Wartell <zwartell@uncc.edu>
2023-07-17 10:47:42 -0400

    -[bug] add patches from ports/vrpn

--- ports/vrpn-quat/portfile.cmake
+++ ports/vrpn-quat/portfile.cmake
@@ -5,11 +5,19 @@
     REF version_07.35
     SHA512 9a4225e01d07f1a298cd1fc5b938e4be57c03b2c82560a2a77cc327a4a83cbde6b88f796fd6071f7bd8de7ebbb6a8bac86b56fa3025697b5bb7820a7cc71862a
     #HEAD_REF version_07.35
+    PATCHES
+        CMakeLists.txt-vrpn.patch
+        CMakeLists.txt-atmellib.patch
+        CMakeLists.txt-gpsnmealib.patch    
 )
 
 set(VRPN_INSTALL TRUE)
 set(VRPN_SUBPROJECT_BUILD)
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}/quat")
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}/quat"
+    OPTIONS
+        -DVRPN_INSTALL=ON
+        -DVRPN_VCPKG=ON
+    )
 vcpkg_cmake_install()
 
 file(INSTALL "${SOURCE_PATH}/README.Legal" DESTINATION "${CURRENT_PACKAGES_DIR}/share/vrpn-quat" RENAME copyright)
